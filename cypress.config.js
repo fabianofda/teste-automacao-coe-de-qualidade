687e84f414de732debbe77ff4803338c3e78a496
Fabiano <fabiano.andrade@inmetrics.com.br>
2024-01-17 11:30:04 -0300

    config allure report

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,15 @@
 const { defineConfig } = require('cypress')
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: "http://www.automationpractice.pl",
     env: {
       apiUrl: "https://api.trello.com/1"
+    },
+    setupNodeEvents(on, config) {
+      allureWriter(on, config);
+      return config;
     },
     chromeWebSecurity: false
   }
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,2 +1,3 @@
 import '../support/commands'
 import 'cypress-plugin-api'
+require('@shelex/cypress-allure-plugin');
